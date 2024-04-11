57c21a18cae40e0e46bb4d34f2d5670c521115bd
danis <ahrnadeev@gmail.com>
2024-04-11 22:06:40 +0300

    meow

--- src/main/java/controllers/ObjectChoose.java
+++ src/main/java/controllers/ObjectChoose.java
@@ -1,21 +1,52 @@
 package controllers;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import static main.gui.Main.data;
 import static main.gui.Main.loadScene;
 
-public class ObjectChoose {
+public class ObjectChoose implements Initializable {
     @FXML
     public Button ocB0, ocB1, ocB2, ocB3;
     @FXML
     public Button mcB0, mcB1, mcB2;
     @FXML
     public Button sB;
+    @FXML
+    public TextField dTF0, dTF1, dTF2;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        dTF0.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
+                data.setLowerBoundary(Double.parseDouble(newValue));
+            }
+        });
+        dTF1.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
+                data.setHigherBoundary(Double.parseDouble(newValue));
+            }
+        });
+        dTF2.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
+                data.setPrecision(Double.parseDouble(newValue));
+            }
+        });
+    }
 
     @FXML
     protected void handleObjectButtonClick(ActionEvent event) {
--- src/main/java/controllers/ResultPage.java
+++ src/main/java/controllers/ResultPage.java
@@ -12,12 +12,17 @@ import static main.gui.Main.data;
 public class ResultPage implements Initializable {
     @FXML
     public Label rpL0, rpL1, rpL2;
+    @FXML
+    public Label rpL3, rpL4, rpL5;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         rpL0.setText("programmeMode: " + data.getProgrammeMode());
         rpL1.setText("objectCode: " + data.getObjectCode());
         rpL2.setText("methodNumber: " + data.getMethodNumber());
+        rpL3.setText("lowerBoundary: " + data.getLowerBoundary());
+        rpL4.setText("higherBoundary: " + data.getHigherBoundary());
+        rpL5.setText("precision: " + data.getPrecision());
     }
 
 }
