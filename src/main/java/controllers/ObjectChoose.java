dd4af946124127eece2cc6d23060f9d9f0303931
danis <ahrnadeev@gmail.com>
2024-04-11 23:33:37 +0300

    meow

--- src/main/java/controllers/ObjectChoose.java
+++ src/main/java/controllers/ObjectChoose.java
@@ -7,8 +7,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import math.equations.EquationFour;
+import math.equations.EquationOne;
+import math.equations.EquationThree;
+import math.equations.EquationTwo;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -22,75 +27,18 @@ public class ObjectChoose implements Initializable {
     @FXML
     public Button ocB0, ocB1, ocB2, ocB3;
     @FXML
+    public Label ocL0, ocL1, ocL2, ocL3;
+    @FXML
     public Button mcB0, mcB1, mcB2;
+    @FXML
+    public Label mcL0, mcL1, mcL2;
     @FXML
     public Button sB;
     @FXML
     public TextField dTF0, dTF1, dTF2;
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-/*        dTF0.textProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
-                data.setLowerBoundary(Double.parseDouble(newValue));
-            }
-        });
-        dTF1.textProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
-                data.setHigherBoundary(Double.parseDouble(newValue));
-            }
-        });
-        dTF2.textProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
-                data.setPrecision(Double.parseDouble(newValue));
-            }
-        });*/
-        //  "-?0[.,][0-9]{1,6} | 0 | -?[1-9]+([.,][0-9]{1,6})?"
-/*        dTF0.focusedProperty().addListener((arg0, oldValue, newValue) -> {
-            if (!newValue) {
-                String text = dTF0.getText();
-                if (text.matches("[+-]?([0-9]*[.,])?[0-9]+")) {
-                    text = text.replace(",", ".");
-                    data.setLowerBoundary(Double.parseDouble(text));
-                } else {
-                    exit("Введенное число не соответствует паттерну!", 1);
-                }
-            }
-        });
-
-        dTF1.focusedProperty().addListener((arg0, oldValue, newValue) -> {
-            if (!newValue) {
-                String text = dTF1.getText();
-                if (text.matches("[+-]?([0-9]*[.,])?[0-9]+")) {
-                    text = text.replace(",", ".");
-                    data.setLowerBoundary(Double.parseDouble(text));
-                } else {
-                    exit("Введенное число не соответствует паттерну!", 1);
-                }
-            }
-        });
-
-        dTF2.focusedProperty().addListener((arg0, oldValue, newValue) -> {
-            if (!newValue) {
-                String text = dTF2.getText();
-                if (text.matches("[+-]?([0-9]*[.,])?[0-9]+")) {
-                    text = text.replace(",", ".");
-                    data.setLowerBoundary(Double.parseDouble(text));
-                } else {
-                    exit("Введенное число не соответствует паттерну!", 1);
-                }
-            }
-        });*/
-    }
-
     @FXML
     protected void handleObjectButtonClick(ActionEvent event) {
-        Node node = (Node) event.getSource();
-        Stage stage = (Stage) node.getScene().getWindow();
-
         if (event.getSource() == ocB0) {
             data.setObjectCode(1);
         } else if (event.getSource() == ocB1) {
@@ -104,9 +52,6 @@ public class ObjectChoose implements Initializable {
 
     @FXML
     protected void handleMethodButtonClick(ActionEvent event) {
-        Node node = (Node) event.getSource();
-        Stage stage = (Stage) node.getScene().getWindow();
-
         if (event.getSource() == mcB0) {
             data.setMethodNumber(1);
         } else if (event.getSource() == ocB1) {
@@ -118,7 +63,6 @@ public class ObjectChoose implements Initializable {
 
     @FXML
     protected void handleSubmitButtonClick(ActionEvent event) {
-
         data.setLowerBoundary(validateBoundary(dTF0.getText()));
         data.setHigherBoundary(validateBoundary(dTF1.getText()));
         data.setPrecision(validatePrecision(dTF2.getText()));
@@ -148,4 +92,15 @@ public class ObjectChoose implements Initializable {
         }
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ocL0.setText(EquationOne.EQUATION);
+        ocL1.setText(EquationTwo.EQUATION);
+        ocL2.setText(EquationThree.EQUATION);
+        ocL3.setText(EquationFour.EQUATION);
+
+        mcL0.setText("Половинного деления");
+        mcL1.setText("Ньютона");
+        mcL2.setText("Простой итерации");
+    }
 }
